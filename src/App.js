a68620f978aa33d12d5678857cc96f705c826bbc
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-11 14:32:15 +0100

    change app to class and create todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Walk dog',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Work on project',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  render() {
+    return (
     <div className="App">
-    <h1>App</h1>
+    <Todos todos={this.state.todos} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
