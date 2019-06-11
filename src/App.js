adf6a058efe1d2fdd1f970b727810762fa5c885c
Nick Playfair <nick@chaosemerald.co.uk>
2019-06-11 16:01:21 +0100

    Use uuid for task id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './components/layout/header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 import './App.css';
 
@@ -8,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Walk dog',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Work on project',
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -41,12 +43,25 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
+  // Add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  }
+
   render() {
     return (
     <div className="App">
-    <Header />
-    <Todos todos={this.state.todos} markComplete={this.markComplete} 
-    delTodo={this.delTodo} />
+      <div className="container">
+        <Header />
+        <AddTodo addTodo={this.addTodo} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} 
+        delTodo={this.delTodo} />
+      </div>
     </div>
     );
   }
